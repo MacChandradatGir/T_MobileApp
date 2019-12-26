d8efb859ba0cca2fbbf5b912843787d527ecc2dd
MacChandradatGir <chandradat.gir@mobileappscompany.us>
2019-12-26 12:26:17 -0500

    update viewmodel

--- app/src/main/java/com/example/t_mobileapp/viewmodel/GitViewModel.kt
+++ app/src/main/java/com/example/t_mobileapp/viewmodel/GitViewModel.kt
@@ -1,11 +1,9 @@
 package com.example.t_mobileapp.viewmodel
 
-import android.app.Activity
 import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.t_mobileapp.adapter.UserAdapter
-import com.example.t_mobileapp.model.Item
 import com.example.t_mobileapp.model.User
 import com.example.t_mobileapp.network.GitFactory
 import com.example.t_mobileapp.view.MainActivity
@@ -25,6 +23,19 @@ class GitViewModel : ViewModel(){
     private val compositeDisposable = CompositeDisposable()
     private lateinit var viewModel: GitViewModel
 
+    //We will use LiveData to hold results from the API Call
+    val userInfo : MutableLiveData<User> = MutableLiveData()
+
+    //NOTE:  Create a function that uses the textwatcher method "onTextChanged"
+    //         to handle the action of the user entering input
+
+    fun onUserNameTextChanged(currentInput: CharSequence, start: Int, before: Int,
+                              count: Int){
+        val currentUserNameEntered = currentInput.toString()
+        makeApiCall(currentUserNameEntered)
+    }
+
+
     fun getUsers(userName : String): Observable<User> {
         return gitFactory.getUsers(userName)
             .observeOn(AndroidSchedulers.mainThread())
@@ -33,12 +44,11 @@ class GitViewModel : ViewModel(){
 
 
     fun makeApiCall(searchEditText: String) {
-        // var searchEditText = search_edittext.text.toString().trim()
 
         compositeDisposable.add(
-            viewModel.getUsers("${searchEditText}")
+            getUsers("${searchEditText}")
                 .subscribe({user ->
-                    mainActivity.displayUsers(user.items)
+                    userInfo.postValue(user)
                 }, {throwable ->
                     Log.e("TAG_ERROR", throwable.toString())
                 })
